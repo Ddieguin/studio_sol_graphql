ab7f772d0ad2101e6de02783b712d0245bc4e28a
Diego Basilio Arruda <diegobasilioarruda@Diegos-MacBook-Pro.local>
2022-12-21 14:57:42 -0800

    fix something about returns in Query

--- src/passwords/passwords.resolver.ts
+++ src/passwords/passwords.resolver.ts
@@ -1,12 +1,12 @@
 import { Resolver, Args, Query } from '@nestjs/graphql';
-import { Rule } from 'src/graphql';
+import { PasswordValidationResponse, Rule } from 'src/graphql';
 import { PasswordsService } from './passwords.service';
 
 @Resolver()
 export class PasswordResolver {
   constructor(private passwordService: PasswordsService) {}
 
-  @Query()
+  @Query(() => PasswordValidationResponse)
   verify(@Args('password') password: string, @Args('rules') rules: Rule[]) {
     return this.passwordService.verify(password, rules);
   }
