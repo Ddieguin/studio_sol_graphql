88c5b696e1d4df384109d3155ff47c22cabb625a
Diego Basilio Arruda <diegobasilioarruda@Diegos-MacBook-Pro.local>
2022-12-23 13:31:02 -0800

    add Dockerfile and Dockercompose

--- src/passwords/passwords.service.ts
+++ src/passwords/passwords.service.ts
@@ -2,14 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { validationFunctions } from '../utils/validation-functions';
 
 @Injectable()
-/**
- * returns { verify: boolean, noMatch: Rule[]}
- */
 export class PasswordsService {
   verify(password: string, rules: Rule[]) {
     const noMatch: string[] = [];
     for (const { rule, value } of rules) {
-      if (!validationFunctions[rule](password, value)) noMatch.push(rule);
+      try {
+        if (!validationFunctions[rule](password, value)) noMatch.push(rule);
+      } catch (e) {
+        throw new Error('the selected rule is invalid');
+      }
     }
     return noMatch.length
       ? { verify: false, noMatch }
